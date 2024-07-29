9d7e2623cf06297226fd7126d2d72f22631c768e
Wall_ <49759454+wall-69@users.noreply.github.com>
2024-07-29 21:12:42 +0200

    I forgor

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,10 @@
         <div class="container pt-3">
             <h1>LaraChat</h1>
             <h2 class="fw-light text-body-secondary">talk, share, connect</h2>
-            <div class="row row-md g-0 g-lg-5 align-items-end">
+            <div class="row row-md g-0 g-lg-5 align-items-center">
                 <div class="col py-5 align-items-start">
                     <form
-                        class="container w-md-75 h-100 ms-xxl-0 rounded-5 d-flex flex-column align-items-center gap-3 px-5 py-3 bg-secondary text-white text-center">
+                        class="container w-md-75 h-100 ms-lg-0 rounded-5 d-flex flex-column align-items-center gap-3 px-5 py-3 bg-secondary text-white text-center">
                         <h3 class="py-3 fw-light">Login to LaraChat</h3>
 
                         <div class="w-100 d-flex flex-column">
@@ -29,7 +29,7 @@
                         </p>
                     </form>
                 </div>
-                <div class="col d-xxl-block d-none">
+                <div class="col d-lg-block d-none">
                     <img src="img/chatting.svg" alt="Image of a mobile phone on which a chat is opened." class="img-fluid">
                 </div>
             </div>
