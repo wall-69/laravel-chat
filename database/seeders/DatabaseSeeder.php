fa6d67d398b43f9f9b0b4f3384b950a26bd17f78
Wall_ <49759454+wall-69@users.noreply.github.com>
2024-09-11 18:44:46 +0200

    whatever

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,5 +25,11 @@ class DatabaseSeeder extends Seeder
             "password" => '$2y$12$cVFF9K.gLEZv.KqpeV0V5OUyaauXPAr41xeZIbb6qv47FRJBfjw3q',
             "profile_picture" => "storage/img/pfp/FghEZYNknYU3iVY4kN7Oq7uSqtMl33GGICwm0AF5.jpg",
         ]);
+        User::factory()->create([
+            'nickname' => 'podnikatel',
+            'email' => 'c@c.c',
+            "password" => '$2y$12$cVFF9K.gLEZv.KqpeV0V5OUyaauXPAr41xeZIbb6qv47FRJBfjw3q',
+            "profile_picture" => "storage/img/pfp/7vTFe9ThR3lYrarHn2J63aLw4BIco466XtoEO0yd.jpg",
+        ]);
     }
 }
