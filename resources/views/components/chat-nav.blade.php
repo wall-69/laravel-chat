76c5782cebfa9985492ea9f1b554a99d2d1b9c59
Wall_ <49759454+wall-69@users.noreply.github.com>
2024-10-01 21:29:00 +0200

    navbar link bold

--- resources/views/components/chat-nav.blade.php
+++ resources/views/components/chat-nav.blade.php
@@ -1,24 +1,39 @@
 <header class="w-100 bg-secondary">
     <nav class="container d-flex py-3 d-flex justify-content-between">
         <span class="h2 fw-light text-white my-auto">LaraChat</span>
+
         <ul class="d-none list-unstyled d-md-flex my-auto gap-5">
-            <li><a href="{{ route('chat.index') }}" class="text-white fs-5 fw-bold">Chat</a></li>
-            <li><a href="#" class="text-white fs-5">Channels</a></li>
+            <li>
+                <a href="{{ route('chat.index') }}" class="text-white fs-5 {{ request()->is('chat') ? 'fw-bold' : '' }}">
+                    Chat
+                </a>
+            </li>
+            <li>
+                <a href="#" class="text-white fs-5 {{ request()->is('channels') ? 'fw-bold' : '' }}">
+                    Channels
+                </a>
+            </li>
             @auth
                 <li>
-                    <a href="{{ route('users.show', ['nickname' => auth()->user()->nickname]) }}" class="text-white fs-5">My
-                        Profile
+                    @php
+                        $userNickname = auth()->user()->nickname;
+                    @endphp
+                    <a href="{{ route('users.show', ['nickname' => $userNickname]) }}"
+                        class="text-white fs-5 {{ request()->is('profile/' . $userNickname) ? 'fw-bold' : '' }}">
+                        My Profile
                     </a>
                 </li>
                 <li>
                     <form action="{{ route('users.logout') }}" method="POST">
                         @method('POST')
                         @csrf
+
                         <button class="bg-transparent border-0 text-white fs-5">Log Out</button>
                     </form>
                 </li>
             @endauth
         </ul>
+
         <button class="d-flex align-items-center justify-content-center d-md-none bg-transparent border-0">
             <i class="bx bx-menu bx-lg text-white"></i>
         </button>
