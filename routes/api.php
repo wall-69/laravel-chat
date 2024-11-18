afcff0c45bc7b006b5e76279b02a2ab7b62f275c
Wall_ <49759454+wall-69@users.noreply.github.com>
2024-11-18 15:22:35 +0100

    API sanctum auth banger

--- routes/api.php
+++ routes/api.php
@@ -10,40 +10,39 @@ use App\Models\Chat;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Chat
-Route::controller(ChatController::class)->name("chats.")->group(function () {
-    Route::middleware(["auth", "web"])->group(function () {
-        // api perhaps?
+// All routes need authentication so do it like this lol
+Route::middleware("auth:sanctum")->group(function () {
+    // Chat
+    Route::controller(ChatController::class)->name("chats.")->group(function () {
         Route::post("/chats/{chat}/join", "join")->name("join");
         Route::post("/chats/{chat}/leave", "leave")->name("leave");
         Route::post("/chats/{chat}/kick", "kick")->name("kick");
 
-        // Should be API???
         Route::patch("/chats/{chat}", "update")->name("update");
         Route::delete("/chats/{chat}", "destroy")->name("destroy");
     });
-});
 
-// ChatAdmin
-Route::controller(ChatAdminController::class)->name("chatAdmins.")->middleware(["auth", "web"])->group(function () {
-    Route::post("/chats/{chat}/change-admin", "changeAdmin")->name("changeAdmin");
-});
+    // ChatAdmin
+    Route::controller(ChatAdminController::class)->name("chatAdmins.")->group(function () {
+        Route::post("/chats/{chat}/change-admin", "changeAdmin")->name("changeAdmin");
+    });
 
-// UserChat
-Route::controller(UserChatController::class)->name("userChats.")->middleware(["auth", "web"])->group(function () {
-    Route::post("/user-chats/{userChat}/last-read", "updateLastRead")->name("updateLastRead");
-});
+    // UserChat
+    Route::controller(UserChatController::class)->name("userChats.")->group(function () {
+        Route::post("/user-chats/{userChat}/last-read", "updateLastRead")->name("updateLastRead");
+    });
 
-// UserChatBan
-Route::controller(UserChatBanController::class)->name("userChatBans.")->middleware(["auth", "web"])->group(function () {
-    Route::post("/user-chat-bans/", "store")->name("store");
-    Route::delete("/user-chat-bans/{userChatBan}", "destroy")->name("destroy");
-});
+    // UserChatBan
+    Route::controller(UserChatBanController::class)->name("userChatBans.")->group(function () {
+        Route::post("/user-chat-bans/", "store")->name("store");
+        Route::delete("/user-chat-bans/{userChatBan}", "destroy")->name("destroy");
+    });
 
 
-// Message
-Route::controller(MessageController::class)->name("messages.")->middleware(["auth", "web"])->group(function () {
-    Route::get("/messages/{chat}", "index")->name("index");
+    // Message
+    Route::controller(MessageController::class)->name("messages.")->group(function () {
+        Route::get("/messages/{chat}", "index")->name("index");
 
-    Route::post("/messages/{chat}", "store")->name("store");
+        Route::post("/messages/{chat}", "store")->name("store");
+    });
 });
